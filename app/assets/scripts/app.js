8c7f89a1de785bf04ed279077e5b3fb626a39bd9
cstultz87 <cstultz@oswego.edu>
2019-06-02 03:04:39 -0500

    shoenice

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '65vw';
+    document.getElementById('main').style.marginLeft = '65vw';
+  }
+
+  function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
