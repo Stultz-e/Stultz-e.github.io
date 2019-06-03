2c1e849e179d8bee23da0fa87744f01155a30b28
cstultz87 <cstultz@oswego.edu>
2019-06-03 01:06:17 -0500

    shoes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,3 +7,12 @@ function openSlideMenu() {
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
   }
+                   
+ var wow = new WOW ({
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       5,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  })
+  wow.init();
